00573ad3a28b08133db0206a35c33e7109b687ce
sine <1311923509@qq.com>
2022-11-27 17:27:01 +0800

    seconde commit

--- config/AutoConfig.go
+++ config/AutoConfig.go
@@ -2,10 +2,12 @@ package config
 
 import (
 	"WebProject/ginEssential/utils/ContactDb"
+	"fmt"
 	"github.com/jmoiron/sqlx"
 )
 
 func AutoConfig() *sqlx.DB {
 	DB := ContactDb.ContactDb()
+	fmt.Println("Test Git")
 	return DB
 }
